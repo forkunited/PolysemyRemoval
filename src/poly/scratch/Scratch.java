2851ae852a735d5cc09637b247eefb920c1f5358
Bill McDowell <forkunited@gmail.com>
2014-12-05 20:32:11 -0500

    ...

--- src/poly/scratch/Scratch.java
+++ src/poly/scratch/Scratch.java
@@ -1,11 +1,20 @@
 package poly.scratch;
 
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import poly.data.annotation.LabelsList;
+import poly.data.annotation.TokenSpansDatum;
+
+import ark.data.Gazetteer;
+import ark.data.annotation.Datum;
+import ark.data.annotation.nlp.PoSTag;
+import ark.data.annotation.nlp.PoSTagClass;
+
 public class Scratch {
 	public static void main(String[] args) {
-		Pattern FACC1Pattern = Pattern.compile("clueweb09\\-([^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*)\t([^\t]*)");
+		/*Pattern FACC1Pattern = Pattern.compile("clueweb09\\-([^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*\t[^\t]*)\t([^\t]*)");
 		//Pattern freebaseTriplePattern = Pattern.compile("<http://rdf\\.freebase\\.com/ns([^>]*)>\t<http://rdf\\.freebase\\.com/ns/type\\.type\\.instance>\t<http://rdf\\.freebase\\.com/ns([^>]*)>\t\\.");
 		//Pattern otherFACC1Pattern = Pattern.compile("[^\\s]*\t[^\\s]*\t[^\\s]*\t[^\\s]*\t[^\\s]*\t([^:]*).*");
 		//Pattern otherOtherFACC1Pattern = Pattern.compile("([^\\s]*)\t([^\\s]*\t[^\\s]*\t[^\\s]*\t[^\\s]*\t[^\\s]*\t[^\\s]*\t[^\\s]*)");
@@ -22,6 +31,99 @@ public class Scratch {
 	
 		StringBuilder x = new StringBuilder();
 		x.append("f").append("y");
-		System.out.println(x.toString());
+		System.out.println(x.toString());*/
+		
+		Scratch x = new Scratch();
+		String beforePattern1 = x.convertPattern(".*{<~/CD} (<p:NNP,NN,JJ,PRP>)+\\,?\\s*(<p:VB,RB>)*<p:VB>(<p:VB,RB>)*{~'that'}{~'because'}(IN|TO|JJ)*", null);
+		System.out.println(beforePattern1);
+	}
+	
+	protected Pattern posTagClassPattern = Pattern.compile("<p:([^>]*)>");
+	protected Pattern gazetteerPattern = Pattern.compile("<g:([^>]*)>");
+	protected Pattern negationPattern = Pattern.compile("\\{~([^\\}]*)\\}");
+	protected Pattern backwardNegationPattern = Pattern.compile("\\{<~([^\\}]*)\\}");
+	protected Pattern conjunctionPattern = Pattern.compile("(\\{[^\\}]*\\}&)*(\\{[^\\}]*\\})");
+	
+	protected String convertPattern(String inputPattern, Datum.Tools<TokenSpansDatum<LabelsList>, LabelsList> datumTools) {
+		
+		// Replace pos tag class references with disjunctions
+		Matcher posTagClassMatcher = this.posTagClassPattern.matcher(inputPattern);
+		while (posTagClassMatcher.find()) {
+			StringBuilder posTagStr = new StringBuilder();
+			String posTagClassesStr = posTagClassMatcher.group(1);
+			String[] posTagClasses = posTagClassesStr.split(",");
+			posTagStr.append("(");
+			for (String posTagClass : posTagClasses) {
+				PoSTag[] posTags = PoSTagClass.fromString(posTagClass);
+				for (PoSTag posTag : posTags)
+					posTagStr.append(posTag).append("|");
+			}
+			posTagStr.delete(posTagStr.length() - 1, posTagStr.length());
+			posTagStr.append(")");
+			inputPattern = inputPattern.replace(posTagClassMatcher.group(), posTagStr.toString());
+			
+			posTagClassMatcher = this.posTagClassPattern.matcher(inputPattern);
+		}
+		
+		// Replace gazetteer references with disjunctions
+		Matcher gazetteerMatcher = this.gazetteerPattern.matcher(inputPattern);
+		while (gazetteerMatcher.find()) {
+			StringBuilder termStr = new StringBuilder();
+			String gazetteerName = gazetteerMatcher.group(1);
+			Gazetteer gazetteer = datumTools.getDataTools().getGazetteer(gazetteerName);
+			termStr.append("(");
+			Set<String> terms = gazetteer.getValues();
+			for (String term : terms) {
+				termStr.append("'").append(term).append("'|");
+			}
+			termStr.delete(termStr.length() - 1, termStr.length());
+			termStr.append(")");
+			inputPattern = inputPattern.replace(gazetteerMatcher.group(), termStr.toString());
+			
+			gazetteerMatcher = this.gazetteerPattern.matcher(inputPattern);
+		}
+		
+		// Replace negations with correct syntax
+		Matcher negationMatcher = this.negationPattern.matcher(inputPattern);
+		while (negationMatcher.find()) {
+			String negatedStr = negationMatcher.group(1);
+			String replacedNegation = "(?!.*" + negatedStr + ")";
+			inputPattern = inputPattern.replace(negationMatcher.group(), replacedNegation);
+			negationMatcher = this.negationPattern.matcher(inputPattern);
+		}
+		
+		// Replace backward negations with correct syntax
+		Matcher backwardNegationMatcher = this.backwardNegationPattern.matcher(inputPattern);
+		while (backwardNegationMatcher.find()) {
+			String negatedStr = backwardNegationMatcher.group(1);
+			String replacedNegation = "(?<!" + negatedStr + ")";
+			inputPattern = inputPattern.replace(backwardNegationMatcher.group(), replacedNegation);
+			negationMatcher = this.backwardNegationPattern.matcher(inputPattern);
+		}
+		
+		// Replace conjunctions with correct syntax
+		Matcher conjunctionMatcher = this.conjunctionPattern.matcher(inputPattern);
+		while (conjunctionMatcher.find()) {
+			StringBuilder conjunctionStr = new StringBuilder();
+			String[] conjuncts = conjunctionMatcher.group().split("&");
+			
+			conjunctionStr.append("(");
+			for (int i = 0; i < conjuncts.length-1; i++) {
+				String conjunctStr = conjuncts[i].substring(1, conjuncts[i].length() - 1);
+				conjunctionStr.append("(?=" + conjunctStr + ")");
+			}
+			
+			String conjunctStr = conjuncts[conjuncts.length - 1].substring(1, conjuncts[conjuncts.length - 1].length() - 1);
+			conjunctionStr.append("(" + conjunctStr + ")");
+			conjunctionStr.append(")");
+			
+			inputPattern = inputPattern.replace(conjunctionMatcher.group(), conjunctionStr.toString());
+			conjunctionMatcher = this.conjunctionPattern.matcher(inputPattern);
+		}
+		
+		inputPattern = inputPattern.replaceAll("([^'A-Z\\$/])([\\$A-Z]+)", "$1([^\\\\s]+/$2[\\\\s]+)");
+		inputPattern = inputPattern.replaceAll("'([^']+)'", "$1/[^\\\\s]+[\\\\s]+");
+		
+		return inputPattern;
 	}
 }

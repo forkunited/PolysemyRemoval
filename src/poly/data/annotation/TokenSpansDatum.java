4dfee9355c33138bb25c16906652d89c69ac3f45
Bill McDowell <forkunited@gmail.com>
2015-02-02 17:57:07 -0500

    ...

--- src/poly/data/annotation/TokenSpansDatum.java
+++ src/poly/data/annotation/TokenSpansDatum.java
@@ -3,7 +3,6 @@ package poly.data.annotation;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
